64be3caabd4f8a4e5112d8fbe5cd6861ced63268
Randy Ochoa <randy708ochoa@gmail.com>
2023-10-13 10:58:09 -0600

    Modificados algunos iconos y agregado boton de DarkMode al proyecto junto con algunas migraciones del light y dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				custom: {
+					one: "#18181B",
+					two: "#f8fafc",
+					three: "#22D3EE",
+				}
+			}
+		},
 	},
 	plugins: [],
 }
