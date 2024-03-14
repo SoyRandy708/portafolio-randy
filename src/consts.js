17d9b0ca775a041cde7d938ef4045f92aba74047
Randy Ochoa <randy708ochoa@gmail.com>
2024-03-14 12:21:00 -0600

    Cambios en los estilos de los iconos de proyectos

---
+++ src/consts.js
@@ -0,0 +1,15 @@
+import IconHTML from "@/Icons/IconHTML.astro";
+import IconCSS from "@/Icons/IconCSS.astro";
+import IconSass from "@/Icons/IconSass.astro";
+import IconTailwind from "@/Icons/IconTailwind.astro";
+import IconJS from "@/Icons/IconJS.astro";
+import IconReact from "@/Icons/IconReact.astro";
+
+export const TECNOLOGIAS_ICONOS = {
+  HTML: IconHTML,
+  CSS: IconCSS,
+  Sass: IconSass,
+  Tailwind: IconTailwind,
+  JS: IconJS,
+  React: IconReact,
+};
