1681db045e691b0d97d3e68a5bccd7eeda448ce5
Randy Ochoa <randy708ochoa@gmail.com>
2024-03-15 16:11:22 -0600

    Cambios de colores

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,24 +5,14 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				custom: {
-					one: "#18181B",
-					two: "#ecfeff",
-					
-					blue: {
-						'50': '#f2f8fd',
-						'100': '#e3f0fb',
-						'200': '#c1e2f6',
-						'300': '#8ac9ef',
-						'400': '#4caee4',
-						'500': '#2594d2',
-						'600': '#1778b5',
-						'700': '#145e90',
-						'800': '#145078',
-						'900': '#164364',
-						'950': '#0f2c42',
-					},			
-				},
+				"custom-white": "#e6f0f7",
+				"primary-white": "#dcf1ff",
+				"secundary-white": "#ca9dfd",
+				"accent-white": "#ad6cf6",
+				"custom-black": "#181a1c",
+				"primary-black": "#2e3239",
+				"secundary-black": "#4aaeffbf",
+				"accent-black": "#0078d9bf",
 			},
 		},
 	},
